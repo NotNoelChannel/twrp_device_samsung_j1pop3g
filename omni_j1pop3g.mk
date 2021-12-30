017bbdb38357361c60834b58e3f40d7ebf1ca9c2
Tri <67373913+TriDiscord@users.noreply.github.com>
2021-12-30 21:24:30 +0300

    TWRP: switch to kernel sources

--- omni_j1pop3g.mk
+++ omni_j1pop3g.mk
@@ -4,9 +4,6 @@ $(call inherit-product, build/target/product/embedded.mk)
 $(call inherit-product, build/target/product/full_base_telephony.mk)
 $(call inherit-product, build/target/product/languages_small.mk)
 
-PRODUCT_COPY_FILES += \
-    device/samsung/j1pop3g/prebuilt/zImage:kernel
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j1pop3g
 PRODUCT_NAME := omni_j1pop3g
