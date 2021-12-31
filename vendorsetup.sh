87ba97c8f3403ad53c4da9843522d2a04876518e
Tri <67373913+TriDiscord@users.noreply.github.com>
2021-12-31 21:28:00 +0300

    TWRP: dynamic vendorsetup.sh (might be reverted)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,8 +5,10 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-PRODUCT="omni"
-DEVICE="j1pop3g"
+PRODUCT="omni" # Change this accordingly
+
+CURRENT=$(dirname $(readlink -f $0))
+DEVICE=$(cut -c19- <<< $(grep -o "ro.product.device=\w*" $CURRENT/default.prop))
 
 for i in eng userdebug; do
 add_lunch_combo ${PRODUCT}_${DEVICE}-${i};
